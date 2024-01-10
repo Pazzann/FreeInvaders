756ea6d2da61dc60d4bdf381fa90ef82cf198d6e
Anton <manton2007@gmail.com>
2024-01-10 12:58:25 +0200

    Add Lives

--- Scripts/GlobalState.cs
+++ Scripts/GlobalState.cs
@@ -5,8 +5,19 @@ public static class GlobalState
     public static float SpeedScale { get; set; } = 1f;
 
     public static int Score { get; set; } = 0;
+    public static int Live { get; set; } = 3;
     public static void AddScore(int score)
     {
         Score += score;
+    }
+
+    public static void AddLive()
+    {
+        Live++;
+    }
+
+    public static void ReduceLive()
+    {
+        Live--;
     }
 }
