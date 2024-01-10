0a7513f3719699e1db27f171942ea2204c3f4a79
egorv <egor.vaskonyan@gmail.com>
2024-01-10 10:10:01 +0100

    frrferfwefrwe

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -5,9 +5,14 @@ namespace FreeInvader.Scripts;
 public partial class Player : Area2D
 {
 	private const float MovementSpeed = 400f;
+
+	private PackedScene _bulletScene;
+	private Marker2D _bulletSpawnPos;
 	
 	public override void _Ready()
 	{
+		_bulletScene = GD.Load<PackedScene>("res://Prefabs/Bullet.tscn");
+		_bulletSpawnPos = GetNode<Marker2D>("BulletSpawnPos")!;
 	}
 
 	public override void _Process(double delta)
@@ -19,5 +24,17 @@ public partial class Player : Area2D
 		
 		if (Input.IsActionPressed("move_right"))
 			GlobalTranslate(Vector2.Right * MovementSpeed * GlobalState.SpeedScale * deltaF);
+	}
+
+	public override void _Input(InputEvent @event)
+	{
+		base._Input(@event);
+
+		if (@event.IsActionPressed("shoot"))
+		{
+			var bullet = (_bulletScene.Instantiate() as Bullet)!;
+			bullet.GlobalPosition = _bulletSpawnPos.GlobalPosition;
+			GetParent().AddChild(bullet);
+		}
 	}
 }
