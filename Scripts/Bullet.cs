d485944a7d335360f6526769fed9441792fc359d
Anton <manton2007@gmail.com>
2024-01-10 10:35:10 +0200

    PushBullet

--- Scripts/Bullet.cs
+++ Scripts/Bullet.cs
@@ -1,15 +1,34 @@
 using Godot;
 using System;
 
-public partial class Bullet : Node
+public partial class Bullet : Area2D
 {
+	private AnimatedSprite2D _sprites;
+	private int _speed = 0;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		_sprites = GetChild<AnimatedSprite2D>(0);
+		_sprites.Frame = 1;
+		if (_sprites.Frame is 1 or 2)
+			_speed *= -1;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		//Position += new Vector2(Position.X, Position.Y + (float)(_speed * delta));
 	}
+	private void OnTimerTimeout()
+	{
+		if (Rotation < 3.0f)
+		{
+			Rotation = Mathf.Pi;
+			return;
+		} 
+		Rotation = 0;
+	}
+
 }
+
+
